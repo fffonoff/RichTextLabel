8f28dd28c56f402bce5106e1bf986d8cc0b379d3
Roman Trifonov <roman1trifonoff@gmail.com>
2023-07-13 21:51:10 +0200

    initial commit

---
+++ RichTextLabel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RichTextLabel
+//
+//  Created by Roman Trifonov on 13/07/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
